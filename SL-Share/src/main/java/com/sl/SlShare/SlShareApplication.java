2a1cce160e9372b59137d79d907241daf9b1b9ff
ike06 <lhq528@163.com>
2020-12-27 17:18:46 +0800

    Initial commit

---
+++ SL-Share/src/main/java/com/sl/SlShare/SlShareApplication.java
@@ -0,0 +1,13 @@
+package com.sl.SlShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlShareApplication.class, args);
+	}
+
+}
---
+++ SL-Share/src/test/java/com/sl/SlShare/SlShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl.SlShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
